86de036b6a986f2dfdf6987a0224151f08bbf777
uk.mani07 <manikandankumar0506@gmail.com>
2026-06-29 06:58:09 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import './assets/style/global.css';
 
 import "./index.css"
 import { HashRouter } from "react-router-dom"
